cff9e827504f773dc7c2f2a3787b2dec8ffb5b01
Marcus Willock <crazcalm@gmail.com>
2014-01-23 16:01:18 -0500

    I need to leave now. I'll test it out on the train.

--- Chapter_1/Fraction.py
+++ Chapter_1/Fraction.py
@@ -1,4 +1,7 @@
 class Fraction:
+	"""
+	A module to deal with basic fractions.
+	"""
 	
 	def __init__(self, top, bottom):
 		
@@ -13,3 +16,41 @@ class Fraction:
 		
 		print (self.num, "/", self.den)
 
+	def __add__(self, otherfraction):
+
+		newnum = self.num * otherfraction.den + \
+				self.den * otherfraction.num
+
+		newdem = self.den * otherfraction.den
+
+		common = gcd(newnum, newden)
+		
+		return Fraction(newnum//common, newden//common)
+
+
+def squareroot(n):
+	"""
+	Newton's Method for square roots
+	"""
+
+	root = n/2	#initial guess will be 1/2 of n
+	for k in range(20):
+		root = (1/2)*(root + (n/root))
+
+	return root
+
+def gcd(m,n):
+	"""
+	Greatest common denominator
+	"""
+
+	while m%n !=0:
+		oldm = m
+		oldn = n
+
+		m = oldn
+		n = oldm %oldn
+
+	return n
+	
+		
