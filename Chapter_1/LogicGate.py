b4a28027d72c18b6575c57584b9f78500abaf711
Marcus Willock <crazcalm@gmail.com>
2014-01-26 02:58:03 -0500

    The Connector is not working, and their modified methods that call non-existed methods is not helping

--- Chapter_1/LogicGate.py
+++ Chapter_1/LogicGate.py
@@ -43,17 +43,48 @@ class BinaryGate(LogicGate):
 		return int(input("Enter Pin B input for gate " + \
 							self.getLabel()+"-->"))
 
+	def setNextPin(self, source):
+		if self.pinA == None:
+			self.pinA = source
+
+		else:
+			if self.pinB == None:
+				self.pinB = source
+			
+			else:
+				raise RuntimeError("Error: NO EMPTY PINS")
+
+
 class UnaryGate(LogicGate):
 	
 	def __init__(self, n):
 		super().__init__(n)
 
-		self.pin = None
+		self.pinA = None
 
-	def getPin(self):
+	#def getPin(self):
 
-		return int(input("Enter Pin iput for gate " + \
-							self.getLabel()+"-->"))
+	#	return int(input("Enter Pin iput for gate " + \
+	#						self.getLabel()+"-->"))
+
+	def setNextPin(self, source):
+		if self.pinA == None:
+			self.pinA = source
+
+		else:
+			if self.pinB == None:
+				self.pinB = source
+			
+			else:
+				raise RuntimeError("Error: NO EMPTY PINS")
+
+	def getPinA(self):
+		if self.pinA == None:
+			return int(input("Enter Pin A input for gate " + \
+						self.getLabel() +"-->"))
+
+		else:
+			return self.pinA.getFrom().getOutput()
 
 class AndGate(BinaryGate):
 
@@ -93,13 +124,27 @@ class NotGate(UnaryGate):
 
 	def performGateLogic(self):
 		
-		a = self.getPin()
+		a = self.getPinA()
 
 		if a ==1:
 			return 0
 		
 		else:
-			return 1	
+			return 1
+
+class Connector:
+		
+	def __init__(self, fgate,tgate):
+		self.fromgate = fgate
+		self.togate = tgate
+
+		tgate.setNextPin(self)
+
+	def getFrom(self):
+		return self.fromgate
+
+	def getTo(self):
+		return self.togate	
 
 
 if __name__ == "__main__":
@@ -118,3 +163,16 @@ if __name__ == "__main__":
 	g3 = NotGate("G3")
 	g3.getOutput()
 	print("Answer: %s\n" % (g3.output))
+
+	print("Testing Connector(s):\n")
+	g1 = AndGate("G1")
+	g2 = AndGate("G2")
+	g3 = OrGate("G3")
+	g4 = NotGate("G4")
+	c1 = Connector(g1,g3)
+	c2 = Connector(g2,g3)
+	c3 = Connector(g3,g4)
+
+	g4.getOutput()
+	print("\nAnd, And, Or, Not Gates")
+	print("Answer: %s" % (g4.output))
