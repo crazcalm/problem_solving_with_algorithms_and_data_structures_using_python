9593dddbf5e9ccb88b74902ef597902d79839f81
Marcus Willock <crazcalm@gmail.com>
2014-01-27 18:14:52 -0500

    Caffeine does wonders for productivity. SumOfN2 is done

--- Chapter_2/Algorithms.py
+++ Chapter_2/Algorithms.py
@@ -1,4 +1,7 @@
+import time
+
 def sumOfN(n):
+
 	theSum = 0
 	for i in range(1, n+1):
 		theSum = theSum + 1
@@ -6,5 +9,27 @@ def sumOfN(n):
 	return theSum
 
 
+def sumOfN2(n):
+
+	start = time.time()	
+
+	theSum = 0
+	for i in range(1, n+1):
+		theSum = theSum + 1
+
+	end = time.time()	
+
+	return theSum, end-start
+
+
 if __name__ == "__main__":
-	print("Testing sumOFN for n = %s Answer: %s" % (10, sumOfN(10)))
+
+	number = 100000
+	answer = sumOfN(number)
+	print("\nTesting sumOfN for n = %d\nAnswer: %d\n" % \
+			(number, answer))
+
+	answer2, timer2 = sumOfN2(number)
+	
+	print("Testing sumOfN2 for n = %d\nAnswer: %d\nTime: %f\n" % \
+			 (number, answer2, timer2))
