7133bf199286326d4685afbe0f627ee193bae7b1
Marcus Willock <crazcalm@gmail.com>
2014-01-28 16:54:09 -0500

    anagramSolution2 (brute force) is done

--- Chapter_2/Algorithms.py
+++ Chapter_2/Algorithms.py
@@ -44,7 +44,6 @@ def anagramSolution1(s1, s2):
 		found = False
 
 		while pos2 < len(alist) and not found:
-			
 			if s1[pos1] == alist[pos2]:
 				found = True
 				
@@ -60,6 +59,26 @@ def anagramSolution1(s1, s2):
 		pos1 += 1
 
 	return stillOK
+
+def anagramSolution2(s1,s2):
+
+	alist1 = list(s1)
+	alist2 = list(s2)
+
+	alist1.sort()
+	alist2.sort()
+
+	pos = 0
+	matches = True
+
+	while pos < len(s1) and matches:
+		if alist1[pos] == alist2[pos]:
+			pos += 1
+
+		else:
+			matches = False
+
+	return matches
 
 
 if __name__ == "__main__":
@@ -102,5 +121,11 @@ if __name__ == "__main__":
 
 	print("anagramSolution1: %s vs %s \nAnswer: %s\n" % \
 			(word1, word3, anagramSolution1(word1,word3)))
+
+	print("anagramSolution2: %s vs %s \nAnswer: %s\n" % \
+			(word1, word2, anagramSolution2(word1,word2)))
+
+	print("anagramSolution2: %s vs %s \nAnswer: %s\n" % \
+			(word1, word3, anagramSolution2(word1,word3)))
 
 	
